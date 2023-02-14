f5393c8e7ecef3b9b900e226faa05a0107cd125c
EmilioHazard <125386109+EmilioHazard@users.noreply.github.com>
2023-02-14 12:31:14 -0600

    Add files via upload

---
+++ Blogg/directorio.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilos.css">
+  <style>
+  </style>
+</head>
+
+<body>
+    <header>
+
+    
+<nav class="navegacion">
+<!-- menu principal -->
+<ul class="menu">
+<!-- imagenes y logos insertados
+-->
+<li>
+<ul> <a href="index.php"><img src="img/Logo Unach Rojo.png" width="100" height="75" alt="">
+</a></ul> </li>
+
+<li><a href="#">Identidad    </a>
+<ul class="submenu">
+<li><a href="directorio.php">Directorio</a></li>
+<li><a href="normatividad.php">Normatividad</a></li>
+<li><a href="colegiados.php">Organos Colegiados</a></li>
+<li><a href="desarrollo.php">Plan de Desarrollo Institucional 2022-2026</a></li>
+<li><a href="informe.php">Informe de Actividades 2022</a></li>
+</ul></li>
+<li><a href="#">Investigacion</a></li>
+<li><a href="#">Publicaicones</a>
+<ul class="submenu2">
+<li><a href="publicaciones.php">Publicaciones de acceso abierto</a></li>
+<li><a href="recursos_pa.php">Recursos para autores</a></li>
+
+</ul></li>
+<li><a href="proyectos.php">Proyectos</a></li>
+<li><a href="estadisitcas.php">Estadisticas</a></li>
+<li><a href="encuestas.php">Encuestas</a>
+<ul class="submenu3">
+<li><a href="#">Encuestas del momento</a></li>
+<li><a href="#">acceder</a></li>
+</ul>
+</li>
+<li><a href="contacto.php">Contacto</a></li>
+<li>
+<ul> <a href="index.php"><img src="img/cesulo.jpeg" width="100" height="75" alt="">
+</a></ul> </li>
+</ul>
+        </nav>
+        <div class="dir">
+    <main class= "conte">
+<img src="img/logo unach.png"  width="350" height="450" alt=""> 
+</main>
+<main class= "dtos">
+<article >
+<h3>Mision</h3>
+<p>Esta es la mision que se traw unununununununss
+</p>
+</article>
+<article >
+<h3>vision</h3>
+<p>Esta es la mision que se traw ununununununun</p>
+</article>
+</main>
+    </div>
+    </header>
+ 
+
+
+    
+</body>
+</html>
+
